707bb3883e1ebc34381077dd864de0046e82cf94
marsht21 <JC8Panthers>
2019-09-12 16:36:04 -0400

    refactor to create PrimeGenerator and NumberPrinter classes

---
+++ src/literateProgramming/PrimeGenerator.java
@@ -0,0 +1,46 @@
+package literateProgramming;
+
+class PrimeGenerator {
+    private int numPrimes;
+
+    public PrimeGenerator(int numPrimes) {
+        this.numPrimes = numPrimes;
+    }
+
+    public int[] generate() {
+        final int ORDMAX = 30;
+        int primes[] = new int[numPrimes + 1];
+
+        primes[1] = 2;
+        int ORD = 2;
+        int nextPrimeSquare = 9;
+
+        int N = 0;
+        int[] multiples = new int[ORDMAX + 1];
+        int candidatePrime = 1;
+        int lastPrimeIndex = 1;
+        while (lastPrimeIndex < numPrimes) {
+            boolean maybePrime;
+            do {
+                  candidatePrime += 2;
+                  if (candidatePrime == nextPrimeSquare) {
+                      ORD++;
+                      nextPrimeSquare = primes[ORD] * primes[ORD];
+                      multiples[ORD - 1] = candidatePrime;
+                  }
+                  N = 2;
+                  maybePrime = true;
+                  while (N < ORD && maybePrime) {
+                      while (multiples[N] < candidatePrime)
+                          multiples[N] += primes[N] + primes[N];
+                      if (multiples[N] == candidatePrime)
+                          maybePrime = false;
+                      N++;
+                  }
+              } while (!maybePrime);
+              lastPrimeIndex++;
+              primes[lastPrimeIndex] = candidatePrime;
+          }
+        return primes;
+    }
+}
